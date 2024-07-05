a234d8e79a74c4f845d7d7df589bfd2d460e6674
Bruno Tejeria <brunotejeria@hotmail.com>
2024-07-05 18:40:38 -0300

    🔧 Add date to Tasks.

--- src/lib/models.ts
+++ src/lib/models.ts
@@ -1,6 +1,7 @@
 // db models
 export type Tasks = {
-    id: string;
-    text: string;
-    status: "pending" | "done";
+  id: string;
+  text: string;
+  status: "pending" | "done";
+  createdAt: Date;
 }
