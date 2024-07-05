d82b611bf9287823f97f0474282553d575860959
Bruno Tejeria <brunotejeria@hotmail.com>
2024-07-05 16:15:49 -0300

    ⚙️ Config files.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
   rules: {
+    "react/react-in-jsx-scope": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
