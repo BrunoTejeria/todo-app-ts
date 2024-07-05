08abf09d6f0476fa062b40ba1d438ba365a8a759
Bruno Tejeria <brunotejeria@hotmail.com>
2024-07-05 18:41:01 -0300

    ⚙️ Format data.ts.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,26 +2,25 @@ import { sql } from "@vercel/postgres";
 import { Tasks } from "./models";
 
 export async function fetchState(id: string) {
-    try {
-        return await sql<Tasks>`SELECT FIRST 1 FROM tasks WHERE id = ${id} `;
-    }
-    catch (e) {
-        console.error(e);
-        return null;
-    }
+	try {
+		return await sql<Tasks>`SELECT FIRST 1 FROM tasks WHERE id = ${id} `;
+	}
+	catch (e) {console.error(e);
+		return null;
+	}
 }
 
 export async function fetchAll(id?: string, text?: string, status?: string) {
-    try {
-        return await sql<Tasks>`SELECT * FROM tasks WHERE
-        id = ${id} OR
-        text = ${text} OR
-        status = ${status}
-        `
-    }
-    catch (e) {
-        console.error(e);
-        return null;
-    }
+	try {
+		return await sql<Tasks>`SELECT * FROM tasks WHERE
+		id = ${id} OR
+		text = ${text} OR
+		status = ${status}
+		`
+	}
+	catch (e) {
+		console.error(e);
+		return null;
+	}
 }
 
