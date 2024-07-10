87ca6bf6d308f32e9197da1af9ef7274245f9b51
Bruno Tejeria <brunotejeria@hotmail.com>
2024-07-10 17:09:07 -0300

    ⚙️ ESlint config.

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,20 @@
+const { rules } = require("eslint-config-prettier");
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  rules: {
+    "indent": ["error", 2]
+  }
+}
--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,17 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import { rules } from "eslint-config-prettier";
+import pkg from 'eslint-config-prettier';
+
+const { rules } = pkg;
+
+
 
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    rules: {
-      "indent": ["error", 2]
-    }
-  },
+  rules,
 ];
